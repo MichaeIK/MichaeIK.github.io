678377fb28e7904d28ba3eab26df121c065b93d3
Michael Korzun <michaelkorzun@Michaels-MacBook-Pro-3.local>
2017-10-13 20:02:59 +0300

    Game X0

--- js/XO.js
+++ js/XO.js
@@ -1,6 +1,7 @@
 $(function() {
 	var arrWinComb = [];
 	var arrGame = ['','','','','','','','',''];
+	var moveCount = 0;
 
 	for(var i=0;i<9;i++){
 		var fild = '<div class="box" data-box-id="'+i+'"></div>';
@@ -8,9 +9,10 @@ $(function() {
 	}
 
 	$(document).on('click', '.box', function() {
+		moveCount++
 		arrGame[$(this).data('box-id')] = '1';
 		$(this).addClass('boxX');
-		randomMove();
+		setTimeout(randomMove, 300);
 		check();
 	})
 
@@ -19,70 +21,31 @@ $(function() {
 		var o = 0;
 		if (arrGame[0] != '' && arrGame[0] == arrGame[1] && arrGame[1] == arrGame[2]){
 		} else if (arrGame[3] != '' && arrGame[3] == arrGame[4] && arrGame[4] == arrGame[5]){
-			if (arrGame[3] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
-		} else if (arrGame[6] != '' && arrGame[6] == arrGame[7] && arrGame[7] == arrGame[8]){
-			if (arrGame[7] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
+		} else if (arrGame[6] != '' && arrGame[6] == arrGame[7] && arrGame[7] == arrGame[8]){	
 		} else if (arrGame[3] != '' && arrGame[0] == arrGame[3] && arrGame[3] == arrGame[6]){
-			if (arrGame[3] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
 		} else if (arrGame[4] != '' && arrGame[1] == arrGame[4] && arrGame[4] == arrGame[7]){
-			if (arrGame[4] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
 		} else if (arrGame[5] != '' && arrGame[2] == arrGame[5] && arrGame[5] == arrGame[8]){
-			if (arrGame[5] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
 		} else if (arrGame[0] != '' && arrGame[0] == arrGame[4] && arrGame[4] == arrGame[8]){
-			if (arrGame[4] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
 		} else if (arrGame[2] != '' && arrGame[2] == arrGame[4] && arrGame[4] == arrGame[6]){
-			if (arrGame[4] === 1) {
-				var x = 1;
-			} else {
-				var o = 1;
-			}
 		}
-		if (x==1){
-			alert('X - Win')
-		} else if (o==1) {
-			alert('0 - Win')
+		if (moveCount === 9){
+			alert('Game over')
 		}
 	}
-
 	function randomMove() {
+		moveCount++
 		var arrRand = [];
 		for (var i = 0; i < arrGame.length; i++) {
 			if (arrGame[i] === '') {
 				arrRand.push(i);
 			}
 		}
-
 		if (arrRand.length === 0) {
 			return false;
 		}
-
 		var randIndex = Math.floor(Math.random() * arrRand.length);
 		arrGame[arrRand[randIndex]] = '0';
-
 		$("#wrap").find("[data-box-id='"+arrRand[randIndex]+"']").addClass('box0');
 	}
+	
 });
