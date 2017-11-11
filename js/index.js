fea0416f78a3369a3afaebcfe2ecb7b764aca641
Michael Korzun <31041089+MichaeIK@users.noreply.github.com>
2017-11-11 17:07:09 +0200

    addContent

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-$(function(){
-	var examples = { 
-			Bootsrap : {title: 'Bootsrap', description: 'Landing page on bootstrap', links: 'link on landing'}, 
-			Materialize : {title: 'Materialize', description: 'Online shop on materialize', links: 'link on online shop'}, 
-			JavaScript{title: {'JavaScript', description: 'Games on JS', links: ['Game with numbers', 'Game XO', 'Todolist', 'Life slider', 'Sea battle'],
-			React: {title: 'React', description: 'Blog on React'}
-		};
-	$('.bootstrap').click(function(){
-		for(let i =0; i<examples.lenght; i++){
-			console.log(examples[i]);
-		}
+// $(function(){
+// 	var click = 0;
+// 	var examples = { 
+// 			Bootsrap : {title: 'Bootsrap', description: 'Landing page on bootstrap', links: 'link on landing'}, 
+// 			Materialize : {title: 'Materialize', description: 'Online shop on materialize', links: 'link on online shop'}, 
+// 			JavaScript: {title: 'JavaScript', description: 'Games on JS', links: ['Game with numbers', 'Game XO', 'Todolist', 'Life slider', 'Sea battle']},
+// 			React: {title: 'React', description: 'Blog on React'}
+// 		};
+// 	$('.bootstrap').click(function(){
+// 		for(let key in examples){
+// 			if (examples[key]['title'] == 'Bootsrap'){
+// 				if (click == 0){
+// 					$(this).class('img').css('position:''position');
+// 					$('.thirdSlide').append('<div class="examples"></div>');
+// 					click++;
+// 				} else {
+// 					$('.examples').remove();
+// 					click = 0;
+// 				}
+// 			}
+// 		}
 
 		
-	})
-	$('.JS').click(function(){
-		// $(.thirdSlide).append()
-		console.log(examples[2])
-	})
-})
+// 	})
+// 	$('.JS').click(function(){
+// 		// $(.thirdSlide).append()
+// 		console.log(examples[2])
+// 	})
+// })
